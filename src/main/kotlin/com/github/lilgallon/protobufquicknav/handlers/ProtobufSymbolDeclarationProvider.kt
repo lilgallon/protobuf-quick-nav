c234f8fd352a5b557db48f76c2a78e0e01b1b3a9
Lilian Gallon <lilian.gallon@4sh.fr>
2025-06-02 03:31:11 +0300

    (feat) rpc - properly implement declaration between proto and generated kotlin files
    working for:
    - RPCs
    
    doc: https://plugins.jetbrains.com/docs/intellij/declarations-and-references.html#declarations

--- src/main/kotlin/com/github/lilgallon/protobufquicknav/handlers/ProtobufSymbolDeclarationProvider.kt
+++ src/main/kotlin/com/github/lilgallon/protobufquicknav/handlers/ProtobufSymbolDeclarationProvider.kt
@@ -8,19 +8,23 @@ import com.intellij.openapi.module.ModuleManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.TextRange
 import com.intellij.protobuf.lang.psi.PbFile
+import com.intellij.protobuf.lang.psi.PbNamedElement
 import com.intellij.protobuf.lang.psi.PbServiceDefinition
 import com.intellij.protobuf.lang.psi.ProtoLeafElement
+import com.intellij.protobuf.lang.psi.impl.PbServiceMethodImpl
 import com.intellij.psi.JavaPsiFacade
 import com.intellij.psi.PsiClass
 import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiMethod
 import com.intellij.psi.search.GlobalSearchScope
+import java.util.Locale.getDefault
 
 @Suppress("UnstableApiUsage")
 class ProtobufSymbolDeclarationProvider : PsiSymbolDeclarationProvider {
-    private fun PbServiceDefinition.getGeneratedClientImplementation(): PsiClass? = getGeneratedClass("GrpcClient")
-    private fun PbServiceDefinition.getGeneratedClientInterface(): PsiClass? = getGeneratedClass("Def")
+    private fun PbNamedElement.getGeneratedClientImplementation(): PsiClass? = getGeneratedClass("GrpcClient")
+    private fun PbNamedElement.getGeneratedClientInterface(): PsiClass? = getGeneratedClass("Def")
 
-    private fun PbServiceDefinition.getGeneratedClass(suffix: String): PsiClass? {
+    private fun PbNamedElement.getGeneratedClass(suffix: String): PsiClass? {
         // com.something.api.rpc.commands becomes com.something.api.commands
         val generatedPackage = (containingFile as? PbFile?)
             ?.packageStatement
@@ -36,6 +40,15 @@ class ProtobufSymbolDeclarationProvider : PsiSymbolDeclarationProvider {
             )
         }
     }
+
+    private fun String.decapitalize(): String = replaceFirstChar { it.lowercase(getDefault()) }
+
+    private fun PbServiceMethodImpl.getGeneratedMethod(): PsiMethod? =
+        // rpc can not have same name, so only one method with same name exist in same service
+        (parent.parent as PbServiceDefinition)
+            .getGeneratedClientInterface()
+            ?.findMethodsByName(name!!.decapitalize(), true)
+            ?.firstOrNull()
 
     private fun findGeneratedClass(
         project: Project,
@@ -72,11 +85,16 @@ class ProtobufSymbolDeclarationProvider : PsiSymbolDeclarationProvider {
     ): Collection<PsiSymbolDeclaration> =
         when (element) {
             is ProtoLeafElement -> when (val parentElement = element.parent) {
-                is PbServiceDefinition -> { // service name
+                is PbServiceDefinition -> {
                     listOfNotNull(
                         parentElement.getGeneratedClientImplementation(),
                         parentElement.getGeneratedClientInterface()
                     ).map { generatedElement -> element.declares(generatedElement) }
+                }
+
+                is PbServiceMethodImpl -> {
+                    listOfNotNull(parentElement.getGeneratedMethod())
+                        .map { generatedElement -> element.declares(generatedElement) }
                 }
 
                 else -> emptyList()
